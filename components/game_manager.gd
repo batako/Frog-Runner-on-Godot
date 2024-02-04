17530f346a0f9a8cef693de43e46d3c3637c9609
batako <guri725ren213@gmail.com>
2024-02-04 16:42:00 +0900

    リンゴを取得すると内部でポイント加算するように

---
+++ components/game_manager.gd
@@ -0,0 +1,9 @@
+class_name GameManager
+
+extends Node
+
+var points = 0
+
+func add_point(point):
+	points += point
+	print(points)
--- scripts/fruit_base.gd
+++ scripts/fruit_base.gd
@@ -2,9 +2,17 @@ class_name FruitBase
 
 extends Area2D
 
+@export var point: int
+
+@onready var GM: GameManager = %GameManager
+
 func _ready() -> void:
+	var script_path = get_script().resource_path
+	assert(point > 0, script_path + ": point を設定してください。")
+	
 	connect("body_entered", _on_body_entered.bind())
 
 func _on_body_entered(body: Node):
 	if body.is_in_group("Player"):
 		queue_free()
+		GM.add_point(point)
