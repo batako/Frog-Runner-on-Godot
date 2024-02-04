3ee23c298ee56b5e10a0e38e9081b85813639d57
batako <guri725ren213@gmail.com>
2024-02-04 15:42:11 +0900

    動作制御
    ・スピード調整
    ・重力調整
    ・動作に合わせてアニメーション切り替え

--- scenes/actors/ninja_frog/ninja_frog.gd
+++ scenes/actors/ninja_frog/ninja_frog.gd
@@ -1,17 +1,29 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED: float = 400.0
+const JUMP_VELOCITY: float = -900.0
+@onready var Sprite = $AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta: float) -> void:
+	# Animations
+	if (velocity.x > 1 || velocity.x < -1):
+		Sprite.animation = "run"
+	else:
+		Sprite.animation = "idle"
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
+		if velocity.y < 0:
+			Sprite.animation = "jump"
+		else:
+			Sprite.animation = "fall"
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -23,6 +35,11 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, 12)
 
 	move_and_slide()
+	
+	# 左に移動中は左に向かす
+	var is_left = velocity.x < 0
+	Sprite.flip_h = is_left
